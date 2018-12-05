660398202eb06300f26a070ee3a4bdcc60e2fe05
Emmanuel Ajiboye <eoa22@njit.edu>
2018-12-05 05:08:18 -0500

    Task:create database for users

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,6 +3,9 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use App\User;
+
 
 class HomeController extends Controller
 {
@@ -25,4 +28,14 @@ class HomeController extends Controller
     {
         return view('home');
     }
+
+    public function profile()
+
+    {
+       $users = User::all();
+
+        return view('profile', ['users' => $users]);
+    }
+
+
 }
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -61,6 +61,11 @@
                                         {{ __('Logout') }}
                                     </a>
 
+                                    <a class="dropdown-item" href="{{ route('profile') }}">Profile</a>
+
+                                    </a>
+
+
                                     <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
                                         @csrf
                                     </form>

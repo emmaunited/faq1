b8e8f29e8e09e2b72e23b80d04e299cd56a3eb49
Emmanuel Ajiboye <eoa22@njit.edu>
2018-12-18 03:04:23 -0500

    Task 7:Create a controller, route, and view to show a contact form and add it to the main menu of your site.

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -5,6 +5,8 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use App\User;
+use App\Car;
+
 
 
 class HomeController extends Controller
--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -2,6 +2,8 @@
 
 use Illuminate\Database\Seeder;
 
+
+
 class DatabaseSeeder extends Seeder
 {
     /**
@@ -12,5 +14,6 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
          $this->call(UsersTableSeeder::class);
+        $this->call(CarsTableSeeder::class);
     }
 }
